28c1a17cbe9344887aaa3155a70fd74c7c4a8698
Ameth <ameth@iMac-de-Ameth.local>
2019-02-27 23:03:16 -0600

    iniciando a experimentar con processing en su version web p5.js

---
+++ js/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas(845, 845);
+    background(48, 57, 82);
+}
+
+function draw() {
+    fill(241, 144, 102);
+    ellipse(20,20,20,20);
+    fill(245, 205, 121);
+    ellipse(30,30,30,30);
+    fill(84, 109, 229);
+    ellipse(50,50,50,50);
+    fill(225, 95, 65);
+    ellipse(80,80,80,80);
+    fill(196, 69, 105);
+    ellipse(130,130,130,130);
+    fill(87, 75, 144);
+    ellipse(210,210,210,210);
+    fill(61, 193, 211);
+    ellipse(340,340,340,340);
+    fill(230, 103, 103);
+    ellipse(550,550,550,550);
+}
